c35ac62c35fb503979aa25e917bea1cbcee0b3ff
Akhmat <latyev.me@gmail.com>
2026-02-18 00:59:23 +0300

    chore(scheduling): setup poms docker postgres and flyway migrations

---
+++ scheduling-app/src/main/java/com/pb/scheduling/SchedulingAppliaction.java
@@ -0,0 +1,11 @@
+package com.pb.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingAppliaction {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingAppliaction.class, args);
+    }
+}
