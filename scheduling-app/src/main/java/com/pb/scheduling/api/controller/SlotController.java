944f0d02c36a059653e4b75dc39998aeeef898b0
Akhmat <latyev.me@gmail.com>
2026-02-18 03:42:29 +0300

    refactor(scheduling): fix swagger testing (gameslot and slotreservation working correctly)

--- scheduling-app/src/main/java/com/pb/scheduling/api/controller/SlotController.java
+++ scheduling-app/src/main/java/com/pb/scheduling/api/controller/SlotController.java
@@ -4,6 +4,7 @@ import com.pb.scheduling.api.dto.request.ConfirmByBookingRequest;
 import com.pb.scheduling.api.dto.request.HoldSlotRequest;
 import com.pb.scheduling.api.dto.response.HoldSlotResponse;
 import com.pb.scheduling.service.SlotService;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,19 +20,25 @@ public class SlotController {
 
     @PostMapping("/slots/{slotId}/holds")
     @ResponseStatus(HttpStatus.CREATED)
-    public HoldSlotResponse hold(@PathVariable UUID slotId, @Valid @RequestBody HoldSlotRequest req) {
+    public HoldSlotResponse hold(
+            @PathVariable("slotId") UUID slotId,
+            @Valid @RequestBody HoldSlotRequest req) {
         return slotService.holdSlot(slotId, req.getBookingId(), req.getHoldMinutes());
     }
 
     @PostMapping("/slot-holds/{slotReservationId}/confirm")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void confirm(@PathVariable UUID slotReservationId, @Valid @RequestBody ConfirmByBookingRequest req) {
+    public void confirm(
+            @PathVariable("slotReservationId") UUID slotReservationId,
+            @Valid @RequestBody ConfirmByBookingRequest req) {
         slotService.confirmHold(slotReservationId, req.getBookingId());
     }
 
     @PostMapping("/slot-holds/{slotReservationId}/cancel")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancel(@PathVariable UUID slotReservationId, @Valid @RequestBody ConfirmByBookingRequest req) {
+    public void cancel(
+            @PathVariable("slotReservationId") UUID slotReservationId,
+            @Valid @RequestBody ConfirmByBookingRequest req) {
         slotService.cancelHold(slotReservationId, req.getBookingId());
     }
 }
